49a9258ad675fd876321c5cc2ffc5e4fdab81abe
Olena Sydorova <olena.sydorova@gmail.com>
2023-06-02 20:49:22 +0200

    add cat-api.js

---
+++ src/index.js
@@ -0,0 +1,72 @@
+import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
+import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select';
+
+const refs = {
+  select: document.querySelector('.breed-select'),
+  catInfo: document.querySelector('.cat-info'),
+  loader: document.querySelector('.loader'),
+  error: document.querySelector('.error'),
+};
+const { select, catInfo, loader, error } = refs;
+
+select.style.display = 'none';
+loader.style.display = 'block';
+error.style.display = 'none';
+
+fetchBreeds()
+  .then(breeds => fillSelect(breeds))
+  .catch(onError);
+
+function fillSelect(breeds) {
+  select.innerHTML = '';
+  loader.style.display = 'none';
+  const catsMarkup = createCatsMarkup(breeds);
+  select.insertAdjacentHTML('beforeend', catsMarkup);
+  select.style.display = 'block';
+}
+
+function createCatsMarkup(data) {
+  return data
+    .map(({ id, name }) => {
+      return ` <option value="${id}">${name}</option>`;
+    })
+    .join('');
+}
+
+function onError(err) {
+  Notiflix.Notify.failure(
+    'Oops! Something went wrong! Try reloading the page!'
+  );
+  console.log('not found');
+}
+// ----------------------------------------------------------
+select.addEventListener('change', e => {
+  catInfo.innerHTML = '';
+  loader.style.display = 'block';
+  const breedId = select.value;
+
+  console.log(breedId);
+  fetchCatByBreed(breedId)
+    .then(cats => {
+      console.log(cats);
+      loader.style.display = 'none';
+
+      const catMarkup = createCatMarkup(cats);
+      catInfo.insertAdjacentHTML('beforeend', catMarkup);
+    })
+    .catch(onError);
+});
+
+function createCatMarkup(cats) {
+  return cats
+    .map(cat => {
+      return `
+        <img src="${cat.url}" width="360" />
+            <h1>${cat.breeds[0].name}</h1>
+            <p>${cat.breeds[0].description}</p>
+            <p><b>Temperament: </b>${cat.breeds[0].temperament}</p>
+        `;
+    })
+    .join('');
+}
---
+++ src/js/cat-api.js
@@ -0,0 +1,24 @@
+const API_KEY =
+  'live_WMvtxePVKRXBjZsjxYFtFlOWqFz0gkyI0mBmoW9aNYlRdNtrxNB85pKa8M2IFrbP';
+const BASE_URL = 'https://api.thecatapi.com/v1';
+
+function fetchBreeds() {
+  return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then(response => {
+    if (!response.ok) {
+      throw new Error('No data');
+    }
+    return response.json();
+  });
+}
+function fetchCatByBreed(breedId) {
+  return fetch(
+    `${BASE_URL}/images/search?breed_ids=${breedId}&api_key=${API_KEY}`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error('No data');
+    }
+    return response.json();
+  });
+}
+
+export { fetchBreeds, fetchCatByBreed };
