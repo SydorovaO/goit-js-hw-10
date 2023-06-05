f1acd26a142d0ec4788303a0f72594b84a9a0300
Olena Sydorova <olena.sydorova@gmail.com>
2023-06-05 19:22:38 +0200

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -3,22 +3,30 @@ const API_KEY =
 const BASE_URL = 'https://api.thecatapi.com/v1';
 
 function fetchBreeds() {
-  return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then(response => {
-    if (!response.ok) {
-      throw new Error('No data');
-    }
-    return response.json();
-  });
+  return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('No data');
+      }
+      return response.json();
+    })
+    .catch(error => {
+      Notiflix.Notify.failure(error.textContent);
+    });
 }
 function fetchCatByBreed(breedId) {
   return fetch(
     `${BASE_URL}/images/search?breed_ids=${breedId}&api_key=${API_KEY}`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error('No data');
-    }
-    return response.json();
-  });
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('No data');
+      }
+      return response.json();
+    })
+    .catch(error => {
+      Notiflix.Notify.failure(error.textContent);
+    });
 }
 
 export { fetchBreeds, fetchCatByBreed };
